09559c4645a1babef9500a362be8b75bf8fb1133
Rupin Bhalla <rupin.bhalla@gmail.com>
2017-01-08 19:48:47 -0500

    Added Other Percentage Capability

--- TipCalculator/TipCalc.php
+++ TipCalculator/TipCalc.php
@@ -16,6 +16,7 @@
 		}
 		$percentage = $_POST['Percentage'] ?? '';
 		$billTotal = $_POST['billTotal'] ?? '';
+		$otherPercentage = $_POST['percentageOther'] ?? '';
 	?>
 
 	<form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method = "post">
@@ -30,23 +31,45 @@
 			{
 				echo('<input type = "radio" name = "Percentage" value = "' . $percentages[$i] . '" >' . $percentages[$i] . '%');	
 			}
+			echo('<input type = "radio" name = "Percentage" id = "otherPercentage"');
+			echo('<label for = "otherPercentage"> Other Percentage</label>');
+
+			echo('<div class = "reveal-if-active">
+
+			    <input type= "text" name= "percentageOther"><br>
+
+			    </div>');
+
 		?>
+
 		<br>
 		<br>
 
 		<?php
+
 		// This is a post request
 		if(isPostRequest())
 		{
 			
-			if(is_numeric($billTotal) && is_numeric($percentage))
+			if(is_numeric($billTotal) && (is_numeric($percentage) || is_numeric($otherPercentage)))
 			{
 				if($billTotal > 0)
 				{
-					$tip = ($percentage / 100) * $billTotal;
-					$billTotal = $billTotal + $tip; 
-					echo('<p>Tip: ' . '$' . $tip . '</p>');
-					echo('Bill Total: ' . '$' . $billTotal);
+					if(is_numeric($percentage))
+					{
+						$tip = ($percentage / 100) * $billTotal;
+						$billTotal = $billTotal + $tip; 
+						echo('<p>Tip: ' . '$' . $tip . '</p>');
+						echo('Bill Total: ' . '$' . $billTotal);
+					}
+					else if(is_numeric($otherPercentage))
+					{
+						$tip = ($otherPercentage / 100) * $billTotal;
+						$billTotal = $billTotal + $tip; 
+						echo('<p>Tip: ' . '$' . $tip . '</p>');
+						echo('Bill Total: ' . '$' . $billTotal);
+					}
+					
 				}
 				else
 				{
